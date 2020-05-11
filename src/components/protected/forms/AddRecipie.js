761cafe996923ce1e2875f91c70edf1a3dfff1ca
Russell <russdow1@hotmail.com>
2020-05-11 00:47:02 -0700

    add steps state handler ready to go

--- src/components/protected/forms/AddRecipie.js
+++ src/components/protected/forms/AddRecipie.js
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faPencilAlt,
+  faSave,
+  faCheck,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
-const AddRecipiePage = ({ingredients, measurements}) => {
-  ingredients = ['egg', 'onion', 'garlic', 'water', 'pepper', 'salt'];
-  let initialSteps = ["mix ingredients", "cook food", "eat"];
-  initialSteps = initialSteps.map((step, i) => step = {'name': step, 'edit': false})
-  
+const AddRecipiePage = ({ ingredients, measurements }) => {
+  ingredients = ["egg", "onion", "garlic", "water", "pepper", "salt"];
+  let initialSteps = [
+    "mix ingredients until fully mixed in the bowl and make sure they're totally mixed like all the way mixed - no joke",
+    "cook food",
+    "eat",
+  ];
+  initialSteps = initialSteps.map(
+    (step, i) => (step = { text: step, edit: false })
+  );
+
   let [steps, setSteps] = useState(initialSteps || []);
 
   const handleAddStep = () => {
-    setSteps([...steps, 'Next Step']);
-  }
+    setSteps([...steps, "Next Step"]);
+  };
   const handleDeleteStep = (i) => {
     let filteredSteps = [...steps];
-    filteredSteps = filteredSteps.slice(0, i).concat(filteredSteps.slice(i + 1, filteredSteps.length));
+    filteredSteps = filteredSteps
+      .slice(0, i)
+      .concat(filteredSteps.slice(i + 1, filteredSteps.length));
     setSteps(filteredSteps);
-  }
+  };
 
   const toggleEditMode = (i) => {
     let editSteps = [...steps];
     let step = editSteps[i];
     step.edit = !step.edit;
     setSteps(editSteps);
-  }
-  
+  };
+
+  const handleStepTextChange = (event, i) => {
+    let text = event.target.value;
+    let stepsList = [...steps];
+    let step = stepsList[i];
+    step.text = text;
+    setSteps(stepsList);
+  };
+
   return (
     <main className="add-recipie-page bg-lg">
-      <div className='add-recipie-header bg-tlg'>
+      <div className="add-recipie-header">
         <h3>Add Recipie</h3>
       </div>
       <form className="form add-recipie-body bg-w">
@@ -41,11 +65,11 @@ const AddRecipiePage = ({ingredients, measurements}) => {
             />
           </div>
 
-          <div className='form-group'>
-            <label for='summary'>Summary:</label>
+          <div className="form-group">
+            <label for="summary">Summary:</label>
             <textarea
-              id='summary'
-              className='form-control'
+              id="summary"
+              className="form-control"
               type="text"
               placeholder="What type of dish is it? Where is it from?
                 Which seasons is it best during? Why do you
@@ -53,62 +77,115 @@ const AddRecipiePage = ({ingredients, measurements}) => {
             />
           </div>
 
-          <label for='servings'>Servings:</label>
-          <input id='servings' className='form-control' type="text" placeholder="Garlic Shrimp" />
+          <label for="servings">Servings:</label>
+          <input
+            id="servings"
+            className="form-control"
+            type="text"
+            placeholder="4 cups"
+          />
 
-          <label for='prep-time'>Prep Time:</label>
-          <input id='prep-time' className='form-control' type="number" placeholder="20" />
+          <label for="prep-time">Prep Time (minutes):</label>
+          <input
+            id="prep-time"
+            className="form-control"
+            type="number"
+            placeholder="20 minutes"
+          />
 
-          <label for='cook-time'>Cook Time:</label>
-          <input id='cook-time' className='form-control' type="number" placeholder="20" />
-
-
+          <label for="cook-time">Cook Time (minutes):</label>
+          <input
+            id="cook-time"
+            className="form-control"
+            type="number"
+            placeholder="20 minutes"
+          />
         </section>
         <section className="ingredients">
-          <div className='form-group'>
-            <label for='ingredients'>Ingredients:</label>
+          <div className="form-group">
+            <label for="ingredients">Ingredients:</label>
 
-            <div className='form-subgroup'>
-              <select id='ingredients' className='form-control'>
+            <div className="form-subgroup">
+              <select id="ingredients" className="form-control">
                 {ingredients.map((ingredient, i) => (
-                  <option key={i} value='ingredient'>{ingredient}</option>
+                  <option key={i} value="ingredient">
+                    {ingredient}
+                  </option>
                 ))}
               </select>
-              <button type='button' className='btn btn-primary'>+ Add</button>
+              <button type="button" className="btn btn-primary">
+                <FontAwesomeIcon icon={faPlus} /> Add
+              </button>
             </div>
           </div>
         </section>
         <section className="preperation-steps">
-          <div className='form-group'>
-            <div className='form-subgroup'>
-              <label for='steps'>Preparation Steps:</label>
-              <button onClick={() => handleAddStep()} type='button' className='btn btn-primary'>+ Add</button>
+          <div className="form-group">
+            <div className="form-subgroup">
+              <label for="steps">Preparation Steps:</label>
+              <button
+                onClick={() => handleAddStep()}
+                type="button"
+                className="btn btn-primary"
+              >
+                <FontAwesomeIcon icon={faPlus} /> Add
+              </button>
             </div>
             <div>
-              <ol className='step-list'>
+              <ol className="step-list">
                 {steps.map((step, i) => (
-                  <li key={i} className={(step.edit ? 'edit' : 'done') + ' step-item'}>
-                    <div className='step-name'>
-                      <span>{step.name}</span>
+                  <li
+                    key={i}
+                    className={(step.edit ? "edit" : "done") + " step-item"}
+                  >
+                    <div className="step-name">
+                      <span>{step.text}</span>
                     </div>
-                    <div className='step-buttons'>
-                      <button onClick={() => handleDeleteStep(i)} type='button' className='btn btn-sm btn-danger delete-btn'>[] Delete</button>
-                      <button onClick={() => toggleEditMode(i)} type='button' className='btn btn-sm btn-success done-btn'>$ Done</button>
-                      <button onClick={() => toggleEditMode(i)} type='button' className='btn btn-sm btn-warning edit-btn'>/ Edit</button>
+                    <div className="step-buttons">
+                      <button
+                        onClick={() => handleDeleteStep(i)}
+                        type="button"
+                        className="btn btn-sm btn-danger delete-btn"
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                      <button
+                        onClick={() => toggleEditMode(i)}
+                        type="button"
+                        className="btn btn-sm btn-success done-btn"
+                      >
+                        <FontAwesomeIcon icon={faCheck} /> Done
+                      </button>
+                      <button
+                        onClick={() => toggleEditMode(i)}
+                        type="button"
+                        className="btn btn-sm btn-warning edit-btn"
+                      >
+                        <FontAwesomeIcon icon={faPencilAlt} />
+                      </button>
                     </div>
-                    {step.edit ? 
-                      <div className='step-text'>
-                        <textarea id={'step-text-' + i} />
-                      </div>
-                      : null
-                    }
+                    {step.edit ? (
+                      <>
+                        <div className="break"></div>
+                        <div className="step-text">
+                          <textarea
+                            onChange={(event) => handleStepTextChange(event, i)}
+                            placeholder='First, mix the ingredients.'
+                            value={step.text}
+                            id={"step-text-" + i}
+                          />
+                        </div>
+                      </>
+                    ) : null}
                   </li>
                 ))}
               </ol>
             </div>
           </div>
         </section>
-        <button type='button' className='btn btn-success btn-block'>Add Recipie</button>
+        <button type="button" className="btn btn-success btn-block">
+          <FontAwesomeIcon icon={faSave} /> Saveg Recipie
+        </button>
       </form>
     </main>
   );
